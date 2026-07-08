c545bf81445e84e3e50ef3958cad880258372d55
yahTawfiq <tawfiq.cspj@gmail.com>
2026-07-08 11:00:16 +0100

    update: hackathon get dto to include status

--- HackathonManager.ws/Application/Hackathons/Dtos/GetHackathonDto.cs
+++ HackathonManager.ws/Application/Hackathons/Dtos/GetHackathonDto.cs
@@ -1,4 +1,6 @@
-﻿namespace HackathonManager.ws.Application.Hackathons.Dtos;
+﻿using HackathonManager.ws.Application.Constants;
+
+namespace HackathonManager.ws.Application.Hackathons.Dtos;
 
 public class GetHackathonDto
 {
@@ -8,5 +10,12 @@ public class GetHackathonDto
     public required string Rules { get; set; }
     public required DateTime StartDate { get; set; }
     public required DateTime EndDate { get; set; }
+    public HackathonStatus Status => DateTime.UtcNow switch
+    {
+        var now when now < StartDate => HackathonStatus.Upcoming,
+        var now when now >= StartDate && now <= EndDate => HackathonStatus.Ongoing,
+        _ => HackathonStatus.Ended,
+    };
+
     public required string EvaluationCriteria { get; set; }
 }
